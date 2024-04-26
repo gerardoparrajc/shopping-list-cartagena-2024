48643fb82d2c4f0ab37d7c7058c2ae258a7357df
Gerardo Parra <gerardoparrajuandelacruz@gmail.com>
2024-04-26 20:57:58 +0200

    Implementado menú para productos y actualización de los productos

--- src/app/components/productos/item-producto/item-producto.component.ts
+++ src/app/components/productos/item-producto/item-producto.component.ts
@@ -5,6 +5,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatDialog } from '@angular/material/dialog';
+import { EditarProductoComponent } from '../../../dialogs/editar-producto/editar-producto.component';
+import { ListasCompraService } from '../../../services/listas-compra.service';
 
 @Component({
   selector: 'app-item-producto',
@@ -22,4 +25,34 @@ import { MatMenuModule } from '@angular/material/menu';
 export class ItemProductoComponent {
   @Input() producto!: Producto;
   @Output() actualizarLista: EventEmitter<void> = new EventEmitter<void>();
+
+  constructor(private dialog: MatDialog, private listasCompraService: ListasCompraService) {
+
+  }
+
+  showDialogEditarProducto() {
+    const dialog = this.dialog.open(EditarProductoComponent, {
+      data: this.producto,
+    });
+
+    dialog.afterClosed().subscribe({
+      next: (result) => {
+        if (result) {
+          this.listasCompraService
+            .updateProductoFromLista(result as Producto)
+            .subscribe({
+              next: (respuesta) => console.log(respuesta),
+              error: (error) => console.log(error),
+            });
+        }
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  showDialogEliminarProducto() {}
+
+  cancelarClick(evento: MouseEvent) {
+    evento.stopPropagation();
+  }
 }
