aa1f0e6752f7bcb0069638491befac987122d838
Gerardo Parra <gerardoparrajuandelacruz@gmail.com>
2024-04-26 19:35:33 +0200

    Creación de componentes necesarios.
    Creación de rutas.
    
    Implementación de plantilla general de la aplicación.
    
    Implementado registro.

---
+++ src/app/components/listas-compra/listas-compra.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listas-compra',
+  standalone: true,
+  imports: [],
+  templateUrl: './listas-compra.component.html',
+  styleUrl: './listas-compra.component.scss'
+})
+export class ListasCompraComponent {
+
+}
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 
 
@@ -14,7 +15,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -22,7 +23,7 @@ export class LoginComponent {
 
   formulario: FormGroup;
 
-  constructor(private auth: AuthenticationService, private snackBar: MatSnackBar) {
+  constructor(private auth: AuthenticationService, private snackBar: MatSnackBar, private router: Router) {
     this.formulario = new FormGroup({
       username: new FormControl('', [Validators.required]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
@@ -44,6 +45,7 @@ export class LoginComponent {
         });
 
         // TODO: Redireccionar a las listas de la compra
+        this.router.navigate(['/']);
       },
       error: (error: any) => {
         let mensaje = '';
