bb4f7409195a6d37c9ceeee7be198fca619f7b4c
Gerardo Parra <gerardoparrajuandelacruz@gmail.com>
2024-04-26 20:20:23 +0200

    Implementado componente para productos.
    Creado componente para item de producto

--- src/app/components/productos/item-producto/item-producto.component.ts
+++ src/app/components/productos/item-producto/item-producto.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Producto } from '../../../models/producto';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-item-producto',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+  ],
   templateUrl: './item-producto.component.html',
-  styleUrl: './item-producto.component.scss'
+  styleUrl: './item-producto.component.scss',
 })
 export class ItemProductoComponent {
-
+  @Input() producto!: Producto;
+  @Output() actualizarLista: EventEmitter<void> = new EventEmitter<void>();
 }
