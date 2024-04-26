66bcbe0f30a310c07d899174797715a3f6d19691
Gerardo Parra <gerardoparrajuandelacruz@gmail.com>
2024-04-26 20:14:41 +0200

    Implementada carga de productos de la lista de la compra.
    Creado comonente para los items de productos.

---
+++ src/app/components/productos/item-producto/item-producto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-producto',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-producto.component.html',
+  styleUrl: './item-producto.component.scss'
+})
+export class ItemProductoComponent {
+
+}
